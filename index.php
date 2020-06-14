483498527481fcecbd6e953d4f66f82a278ddc13
Andipol <andi@data.pl>
2020-06-14 15:57:36 +0200

    New JS files and changes in HTML

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
-<!DOCTYPE html>
-<html>
-	<head>         
-        
-    </head>
-	<body>
+<!DOCTYPE HTML>
+<HTML>
+<HEAD>
+        <META charset="utf-8">
+        <META HTTP-EQUIV="Content-Language" CONTENT="pl">
+        <TITLE>Game</TITLE>
+</HEAD>	
+<BODY>	
+	<script src="jquery-3.3.1.min.js"></script>
 	<script src="game.js"></script>
-	</body>
-</html>
+</BODY>
+</HTML>
