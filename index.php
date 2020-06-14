e5e96e114ad41d7c3be3f40db83826d6f1f14154
Andipol <andi@data.pl>
2020-06-14 19:29:21 +0200

    New CSS file with new modyfication in JS and HTML

--- index.php
+++ index.php
@@ -1,11 +1,39 @@
-<!DOCTYPE HTML>
+﻿<!DOCTYPE HTML>
 <HTML>
 <HEAD>
         <META charset="utf-8">
         <META HTTP-EQUIV="Content-Language" CONTENT="pl">
+		<link rel="stylesheet" href="style.css">
         <TITLE>Game</TITLE>
 </HEAD>	
 <BODY>	
+	<h2>Animated Modal with Header and Footer</h2>
+
+<!-- Trigger/Open The Modal -->
+<button id="myBtn">Open Modal</button>
+
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <div class="modal-header">
+      <span class="close">&times;</span>
+      <h2>Modal Header</h2>
+    </div>
+    <div class="modal-body">
+      <p>Some text in the Modal Body</p>
+      <p>Some other text...</p>
+	  <p><img src="crystal.png"></p>
+    </div>
+    <div class="modal-footer">
+      <h3>Modal Footer</h3>
+    </div>
+  </div>
+
+</div>
+
+	
 	<script src="jquery-3.3.1.min.js"></script>
 	<script src="game.js"></script>
 </BODY>
